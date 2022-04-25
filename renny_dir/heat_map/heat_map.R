58ef3c809b61745af780f8052c3111010de1a9b2
rennydoig <rennyd@sfu.ca>
2022-04-25 10:57:28 -0700

    Adding code for leaflet map

--- renny_dir/heat_map/heat_map.R
+++ renny_dir/heat_map/heat_map.R
@@ -16,25 +16,29 @@ data <- st_read("../ookla-canada-speed-tiles.shp")
 
 # function for merging year and quarter into one time variable
 merge_time <- Vectorize(function(year, quarter){
-  year + (as.numeric(substr(quarter, 2, 2))-1)/4 - 2019})
+  as.numeric(year) + (as.numeric(substr(quarter, 2, 2))-1)/4 - 2019})
 
 
 
 # Reformat data ----------------------------------------------------------------
 
+data <- st_transform(data, "+init=epsg:4326")
+
 data_lf <- data %>%
   mutate(time = merge_time(year, quarter),
          avg_d_mbps = avg_d_kbps / 1024,
          avg_u_mbps = avg_u_kbps / 1024) %>%
-  filter(time == 0) %>%
-  select(geometry, avg_d_mbps)
+  filter(time == 0)
 
 
 
 # Generate heat map ------------------------------------------------------------
 
-lf <- leaflet()
+pal <- colorNumeric(palette="Blues", domain=data_lf$avg_d_mbps)
 
+leaflet()
+  addTiles() %>%
+  addPolygons(data=data_lf, stroke=F, smoothFactor=0.2, fillOpacity=1, color= ~pal(avg_d_mbps))
 
 
 
