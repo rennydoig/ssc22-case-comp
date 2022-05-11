ac1517a093cd21cfbdcef941d81a84b4343f5750
rennydoig <rennyd@sfu.ca>
2022-05-11 15:53:10 -0700

    adding options for connection time, population centre, and region to table

--- renny_dir/shiny_app/shiny_app_no_map.R
+++ renny_dir/shiny_app/shiny_app_no_map.R
@@ -20,12 +20,13 @@ target_d <- 50
 target_u <- 10
 
 # covariates should have a row for each value of region 
-get_table <- function(region, covariates=NULL){
+get_aggregate_table <- function(region, covariates=NULL){
   temp <- data %>%
+    filter(year==2021 && quarter=="Q4") %>%
     mutate(avg_d_mbps = avg_d_kbps / 1024,
            avg_u_mbps = avg_u_kbps / 1024,
            .keep = "unused") %>%
-    group_by(across(all_of(c("conn_type", "year", "quarter", region)))) %>%
+    group_by(across(all_of(c("conn_type", region)))) %>%
     summarise(avg_d_mbps = sum(tests * avg_d_mbps) / sum(tests),
               avg_u_mbps = sum(tests * avg_u_mbps) / sum(tests),
               prop_d = min(avg_d_mbps / target_d, 1),
@@ -38,6 +39,31 @@ get_table <- function(region, covariates=NULL){
   return(temp)
 }
 
+get_summary_table <- function(region, conn, inc_urban){
+  
+  if(inc_urban){
+    temp <- data
+  } else {
+    temp <- filter(data, is.na(PCCLASS))
+  }
+  
+  if(conn==1){
+    temp <- filter(temp, conn_type=="mobile")
+  } else if(conn==2){
+    temp <- filter(temp, conn_type="fixed")
+  }
+  
+  temp %>%
+    filter(year==2021 & quarter=="Q4") %>%
+    mutate(d_thresh = avg_d_kbps >= target_d * 1024,
+           u_thresh = avg_u_kbps >= target_u * 1024,
+           .keep = "unused") %>%
+    group_by(across(all_of(c("conn_type", region)))) %>%
+    summarise(prop_u = mean(d_thresh),
+              prop_d = mean(u_thresh),
+              .groups="keep")
+}
+
 
 
 # Create the shiny app ---------------------------------------------------------
@@ -48,13 +74,15 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      radioButtons("region",
+                   "Summary Region",
+                   list(`Census Division`="CDUID", `Disemination Area`="DAUID", `Province`="PRUID")),
+      radioButtons("connection",
+                   "Connection Type",
+                   list(`Mobile`=1, `Fixed`=2, `Both`=3)),
+      radioButtons("pop_centre",
+                   "Include urban regions?",
+                   list(Yes=T, No=F))
       
     ),
     
@@ -62,20 +90,23 @@ ui <- fluidPage(
     mainPanel(
       
       # Output: Histogram ----
-      # DT::dataTableOutput("table")
-      plotOutput("histo")
+      DT::dataTableOutput("table"),
+      textOutput("test")
       
     )
   )
 )
 
-server <- function(intput, output, session){
-  tb <- get_table("CDUID")
-  # tb <- head(iris)
-  output$table <- DT::renderDataTable(datatable(head(tb)))
+server <- function(input, output, session){
+  # tb <- get_table("CDUID")
+  output$test <- renderText(input$region)
+  output$table <- DT::renderDataTable({
+    datatable(get_summary_table(input$region, input$connection, input$pop_centre))
+  })
   
-  output$histo <- renderPlot(histogram(rnorm(100, bins=intput$bins)))
+  # output$histo <- renderPlot(hist(rnorm(100, bins=intput$bins)))
 }
+
 
 shinyApp(ui = ui, server=server)
 
