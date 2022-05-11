1e03be2f84e6635ba278a93a16db676b173a27eb
Renny Doig <rennyd@sfu.ca>
2022-05-10 17:11:40 -0700

    Code for grouping sf files

--- renny_dir/heat_map/heat_map.R
+++ renny_dir/heat_map/heat_map.R
@@ -18,11 +18,14 @@ data <- st_read("../ookla-canada-speed-tiles.shp")
 merge_time <- Vectorize(function(year, quarter){
   as.numeric(as.character(year)) + (as.numeric(substr(quarter, 2, 2))-1)/4 - 2019})
 
+# directory for grouped shapefiles
+group_dir <- "../shapefiles_by_grouping/"
 
 
 # Reformat data ----------------------------------------------------------------
 
 data <- st_transform(data, "+init=epsg:4326")
+
 
 data_lf <- data %>%
   mutate(time = merge_time(year, quarter),
@@ -30,6 +33,37 @@ data_lf <- data %>%
          avg_u_mbps = avg_u_kbps / 1024) %>%
   filter(time == 0)
 
+
+# generate and save grouped shapefiles
+groupings <- c("DAUID", "CDUID")
+for( gr in groupings )
+{
+  cat("Grouping by", gr)
+  # gr_id <- filter(data, PRUID==10)[c(gr,"conn_type")]
+  temp <- data %>%
+    select(c(avg_d_kbps, avg_u_kbps, tests, year, quarter, conn_type, gr)) %>%
+    mutate(time = merge_time(year, quarter), .keep="unused") %>%
+    # aggregate(gr_id, function(x) c(sum(x[1:3]), x[4:5]))
+    group_by(across(all_of(c("conn_type", "time", gr)))) %>%
+    summarise(avg_d_mbps = sum(tests * avg_d_kbps) / (1024*sum(tests)),
+              avg_u_mbps = sum(tests * avg_u_kbps) / (1024*sum(tests)),
+              .groups = "keep")
+  st_write(temp, paste0(group_dir, gr, ".shp",))
+  cat("- Done. \n")
+}
+
+
+data_agg <- data %>%
+  select(c(avg_d_kbps, avg_u_kbps, tests, year, quarter, conn_type, CDUID, 
+           DAUID, PRUID, SACTYPE, DA_POP, PCUID, PCTYPE, PCCLASS)) %>%
+  mutate(avg_d_mbps = avg_d_kbps / 1024,
+         avg_u_mbps = avg_u_kbps / 1024,
+         time = merge_time(year, quarter),
+         .keep = "unused") %>%
+  group_by(conn_type, time, CDUID) %>%
+  summarise(avg_d_mbps = sum(tests * avg_d_mbps) / sum(tests),
+            avg_u_mbps = sum(tests * avg_u_mbps) / sum(tests),
+            .groups="keep")
 
 
 # Generate heat map ------------------------------------------------------------
@@ -47,13 +81,12 @@ base_map <- leaflet(canada) %>%
 
 # add our polygon data to the map
 
-data_temp <- filter(data_lf, PRUID==10)
+pal <- colorNumeric(palette="Reds", domain=temp$avg_d_mbps)
 
-pal <- colorNumeric(palette="Blues", domain=data_temp$avg_d_mbps)
-
-leaflet() %>%
+leaflet(filter(temp, time==0)) %>%
   addTiles() %>%
-  addPolygons(data=data_temp, stroke=F, smoothFactor=0.2, fillOpacity=1, color= ~pal(avg_d_mbps))
+  addPolygons(color= ~pal(avg_d_mbps), weight=2, highlight=highlightOptions(weight = 5, color = "white",
+                                                                            bringToFront = TRUE))
 
 
 
